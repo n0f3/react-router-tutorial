a1d1063cb9c58780102fbbce6b7c3e43b39b03b5
Alessandro <alessandro@playstudios.com>
2016-07-13 15:40:28 -0700

    Finished lesson 4

--- lessons/03-navigating-with-link/modules/App.js
+++ lessons/03-navigating-with-link/modules/App.js
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1>Ghettohub Issues</h1>
         <ul role="nav">
           <li><Link to="/about">About</Link></li>
           <li><Link to="/repos">Repos</Link></li>
